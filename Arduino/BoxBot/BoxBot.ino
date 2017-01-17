41445d0e42a0cf7cc8f126cd5b16dd9ec56fcb06
Arnout Schekkerman <arnout-@hotmail.com>
2017-01-17 19:29:31 +0100

    js working
    js directions get sent to the poem field to be collected by the nodemcu

---
+++ Arduino/BoxBot/BoxBot.ino
@@ -0,0 +1,268 @@
+#include <ESP8266HTTPClient.h>
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h> 
+
+class SpringyValue 
+{
+  public:
+    float x = 0, v = 0, a = 0, // x (value), v (velocity) and a (acceleration) define the state of the system
+          o = 0, // o defines the temporary spring offset w.r.t. its resting position
+          c = 20.0, k = 1.5, m = 1.0; // c (spring constant), k (damping constant) and m (mass) define the behavior of the system
+
+    // Perturb the system to change the "length" of the spring temporarlily
+    void perturb(float offset) {
+      this->o = offset;
+    }
+
+    // Call "update" every now and then to update the system
+    // parameter dt specifies the elapsed time since the last update
+    void update(float dt) {
+      a = (-c * x - k * v ) / m;
+      v += a * dt;
+      x += v * dt + o;
+      o = 0; // a spring offet only takes one frame
+    }
+};
+
+
+#define BUTTON_PIN  D1
+#define PIN         D2   
+#define LED_COUNT    6
+
+#define fadeInDelay  5
+#define fadeOutDelay 8
+
+#define requestDelay 2000
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ400);
+Servo myServo;
+
+int oldTime = 0;
+int oscillationTime = 500;
+String chipID;
+char chipIdArray[5] = {};
+String webURL = "http://thingscon16.futuretechnologies.nl";
+
+void setAllPixels(uint8_t r, uint8_t g, uint8_t b, float multiplier);
+
+void setup() 
+{
+  configureChipID();
+  strip.begin();
+  strip.setBrightness(255);
+  WiFiManager wifiManager;
+  Serial.begin(115200);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  int counter = 0;
+  while(digitalRead(BUTTON_PIN) == LOW)
+  {
+    counter++;
+    delay(10);
+
+    if(counter > 500)
+    {
+      wifiManager.resetSettings();
+      Serial.println("Remove all wifi settings!");
+      setAllPixels(255,0,0,1.0);
+      fadeBrightness(255,0,0,1.0);
+      ESP.reset();
+    }
+  }
+  delay(1000);
+
+  Serial.println();
+  Serial.print("Last 2 bytes of chip ID: ");
+  Serial.println(chipID);
+
+  String wifiNameConcat = "BoxBot_" + chipID;
+  char wifiName[19] = {};
+  wifiNameConcat.toCharArray(wifiName, 19);
+  
+  setAllPixels(0,255,255,1.0);
+  wifiManager.autoConnect(wifiName);
+  fadeBrightness(0,255,255,1.0);
+  myServo.attach(D7);
+}
+
+void setAllPixels(uint8_t r, uint8_t g, uint8_t b, float multiplier = 1.0) 
+{
+  for (int iPixel = 0; iPixel < LED_COUNT; iPixel++)
+    strip.setPixelColor(iPixel,
+                        (byte)((float)r * multiplier),
+                        (byte)((float)g * multiplier),
+                        (byte)((float)b * multiplier));
+  strip.show();
+}
+
+//This method starts an oscillation movement in both the LED and servo
+void oscillate(float springConstant, float dampConstant, int c)
+{
+  SpringyValue spring;
+  
+  byte red = (c >> 16) & 0xff;
+  byte green = (c >> 8) & 0xff;
+  byte blue = c & 0xff;
+  
+  spring.c = springConstant;
+  spring.k = dampConstant / 100;
+  spring.perturb(255);
+
+  //Start oscillating
+  for(int i = 0; i < oscillationTime; i++)
+  {
+    spring.update(0.01);
+    setAllPixels(red, green, blue, abs(spring.x) / 255.0);
+    myServo.write(90 + spring.x/4); 
+
+    //Check for button press
+    if(digitalRead(BUTTON_PIN) == LOW)
+    {
+      //Fade the current color out
+      fadeBrightness(red, green, blue, abs(spring.x) / 255.0);
+      return;
+    }
+    delay(10);
+  }
+  fadeBrightness(red, green, blue, abs(spring.x) / 255.0);
+}
+
+//This method grabs the current RGB values and current brightness and fades the colors to black
+void fadeBrightness(uint8_t r, uint8_t g, uint8_t b, float currentBrightness)
+{
+    for(float j = currentBrightness; j > 0.0; j-=0.01)
+    {
+          setAllPixels(r, g, b, j);
+          delay(20);
+    }
+    hideColor();
+}
+
+void loop() 
+{
+  //Check for button press
+  if(digitalRead(BUTTON_PIN) == LOW)
+  {    
+    sendButtonPress();
+    delay(250);
+  }
+
+  //Every requestDelay, send a request to the server
+  if(millis() > oldTime + requestDelay)
+  {
+    requestMessage();
+    oldTime = millis();
+  }
+}
+
+void sendButtonPress()
+{
+    Serial.println("Sending button press to server");
+    HTTPClient http;
+    http.begin(webURL + "/api.php?t=sqi&d=" + chipID);
+    uint16_t httpCode = http.GET();      
+    http.end();
+}
+
+void requestMessage()
+{
+  Serial.println("Sending request to server");
+  hideColor();
+      
+  HTTPClient http;
+  http.begin(webURL + "/api.php?t=gqi&d=" + chipID + "&v=2");
+  uint16_t httpCode = http.GET();
+
+  if (httpCode == 200) 
+  {
+    String response;
+    response = http.getString();
+    //Serial.println(response);
+
+    if(response == "-1")
+    {
+      Serial.println("There are no messages waiting in the queue");
+    }
+    else
+    {
+      //Get the indexes of some commas, will be used to split strings
+      int firstComma = response.indexOf(',');
+      int secondComma = response.indexOf(',', firstComma+1);
+      int thirdComma = response.indexOf(',', secondComma+1);
+      
+      //Parse data as strings
+      String hexColor = response.substring(0,7);
+      String springConstant = response.substring(firstComma+1,secondComma);
+      String dampConstant = response.substring(secondComma+1,thirdComma);;
+      String message = response.substring(thirdComma+1,response.length());;
+
+      Serial.println("Message received from server: \n");
+      Serial.println("Hex color received: " + hexColor);
+      Serial.println("Spring constant received: " + springConstant);
+      Serial.println("Damp constant received: " + dampConstant);
+      Serial.println("Message received: " + message);
+
+      //Extract the hex color and fade the led strip
+      int number = (int) strtol( &response[1], NULL, 16);
+      oscillate(springConstant.toFloat(), dampConstant.toFloat(), number);
+    }
+  }
+  else
+  {
+    ESP.reset(); 
+  }
+    
+  http.end();
+}
+
+void hideColor() 
+{
+  colorWipe(strip.Color(0, 0, 0));
+}
+
+void colorWipe(uint32_t c) 
+{
+  for(uint16_t i=0; i<strip.numPixels(); i++) 
+  {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+}
+
+void configureChipID()
+{
+  uint32_t id = ESP.getChipId();
+  byte lower = id & 0xff;
+  byte upper = (id >> 8) & 0xff;
+
+  String l = "";
+  String u = "";
+
+  if(lower < 10)
+  {
+    l = "0" + String(lower, HEX);
+  }
+  else
+  {
+    l = String(lower, HEX);
+  }
+
+  if(upper < 10)
+  {
+    u = "0" + String(upper, HEX);
+  }
+  else
+  {
+    u = String(upper, HEX);
+  }
+ 
+  chipID = u + l;
+  chipID.toUpperCase();
+  chipID.toCharArray(chipIdArray, 5);
+}
+
+
--- Website/BoxBot/public_html/js/boxControll.js
+++ Website/BoxBot/public_html/js/boxControll.js
@@ -3,19 +3,24 @@
 var recordedDirections = "";
 var speed = 25; //25 out of 100
 var time = 500; //in ms
-var internalDevider = '&';  // looks like "forward+25+500;left+25+500;"
-var devider = ';';  //ends single direction command
+var internalDevider = '_';  // looks like "forward+25+500;left+25+500;"
+var devider = ';';  //ends a single direction command
 var formattedDirection = "";    //direction after formatting
-var recordedDirections = "";    //record of all direction commands given
-var num = 0; //counts the amount of commands
+var num = 0; //is used for the commands array, gives the images a unique id for removal
+var recordedDirections = "";
+var directionArray = new Array();
 
 
 function addDirection(direction) {
     console.log(direction);
     formattedDirection = direction + internalDevider + speed + internalDevider + time + devider; // so it will look like "forward+25+500;left+25+500;" or "direction+speed+time;"
-    console.log(recordedDirections);
-    num = num+1;
+    directionArray[num] = formattedDirection;
+    console.log(directionArray);
 
+    //num + 1 after everything is done
+    num = num + 1;
+
+    //add image
     var img = document.createElement("img");
 
     switch (direction) {
@@ -32,27 +37,66 @@ function addDirection(direction) {
             img.src = "resources/directions/down.png";
             break;
     }
-    img.id = "directionImage" + num;
+    img.id = "directionImage" + num; //give the image an unique id for removal
     img.height = 50;
     img.width = 50;
-    document.getElementById("directionsField").appendChild(img);
-    console.log(img.id);
+    document.getElementById("directionsField").appendChild(img); //append image to div
 }
 
 function erase() {
-    //command removal
-    recordedDirections = recordedDirections - formattedDirection;
-    
-    //image removal
-    $("#directionImage" + num).remove();// num causes the last added image to be removed instead of the first
-    num = num -1; //prepare num to delete the second last image
-    console.log(recordedDirections);
+    if (num > 0) {//prevents num from going negative, otherwise the array will be negative
+        //command removal
+        delete directionArray[num - 1]; //minus 1 becaus the array is incremented after filling an element
+
+        //image removal
+        $("#directionImage" + num).remove();// num causes the last added image to be removed instead of the first
+
+        num = num - 1; //prepare num to delete the second last image
+
+        console.log(directionArray);
+    }
 }
 
 function go() {
+    var recordedDirections = "";
+    for (var i = 0; i <= num; i++) {//add all the array entries together into one string to be sent
+        if (directionArray[i] != null) {//prevents adding empty array entries
+            recordedDirections = recordedDirections + directionArray[i];
+        }
+    }
+    console.log(recordedDirections);
 
+    if (directionArray[0] != null) {//if the first enry is not empty
+        //post recordedDirections to wilco's api and then to the nodeMCU
+        url = "http://thingscon16.futuretechnologies.nl/api.php?t=sdc&d=FF6E&td=FF6E&c=&m=" + recordedDirections;
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: recordedDirections,
+            success: console.log("go post success")
+        });
+
+        //change button to stop button
+        document.getElementById("goButton").className = 'btn btn-danger controllerButton'; //change class to change color
+        document.getElementById("goButton").setAttribute("onClick", "stop();"); //change onclick event to stop to run stop at the next click
+        document.getElementById("goButton").id = 'stopButton'; //change id to use js on the stop button instead of the go button
+        document.getElementById("goButtonGlyphicon").className = 'glyphicon glyphicon-stop'; //change glypicon to stop
+    }
 }
 
 function stop() {
+    //to stop the bot
+    $.ajax({
+        type: "POST",
+        url: "http://thingscon16.futuretechnologies.nl/api.php?t=sdc&d=FF6E&td=FF6E&c=&m=stop",
+        data: stop,
+        success: console.log("stop post success")
+    });
+    
+    //change button to goButton
+    document.getElementById("stopButton").className = 'btn btn-success controllerButton'; //change class to change color
+    document.getElementById("stopButton").setAttribute("onClick", "go();"); //change onclick event to go to run go at the next click
+    document.getElementById("stopButton").id = 'goButton'; //change id to use js on the go button instead of the stop button
+    document.getElementById("goButtonGlyphicon").className = 'glyphicon glyphicon-play-circle'; //change glypicon to go
 
 }
